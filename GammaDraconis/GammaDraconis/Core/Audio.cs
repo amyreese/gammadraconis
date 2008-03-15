ca6c40f292e66ccc00ed09db9603585b2b88dbb7
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-03-15 02:37:39 +0000

    Ported input and audio managers from SnailsPace.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@955 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- GammaDraconis/GammaDraconis/Core/Audio.cs
+++ GammaDraconis/GammaDraconis/Core/Audio.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 
 namespace GammaDraconis.Core
 {
@@ -10,9 +11,171 @@ namespace GammaDraconis.Core
     /// </summary>
     class Audio
     {
+        // XACT components.
+        AudioEngine audioEngine;
+        SoundBank soundBank;
+        WaveBank waveBank;
+
+        Dictionary<String, Cue> cues;
+        Dictionary<String, bool> repeat;
+
         /// <summary>
-        /// Update the audio manager's state.
+		/// Create the appropriate XACT components.
+		/// </summary>
+        public Audio()
+        {
+            audioEngine = new AudioEngine("Resources/Audio/Engine.xgs");
+            soundBank = new SoundBank(audioEngine, "Resources/Audio/Audio.xsb");
+            waveBank = new WaveBank(audioEngine, "Resources/Audio/Audio.xwb");
+
+            cues = new Dictionary<string,Cue>();
+            repeat = new Dictionary<string,bool>();
+        }
+
+        /// <summary>
+		/// Fire-and-forget sound.
         /// </summary>
-        public void update() { }
+        /// <param name="cue"></param>
+        public void play(String cue) { play(cue, true); }
+        public void play(String cue, bool overlap)
+        {
+            cache(cue);
+            if (cues[cue].IsPrepared)
+            {
+                cues[cue].Play();
+            }
+            else if (cues[cue].IsPaused)
+            {
+                cues[cue].Resume();
+            }
+            else if (overlap)
+            {
+                recache(cue);
+                cues[cue].Play();
+            }
+        }
+
+        /// <summary>
+		/// Start a repeating sound.
+        /// </summary>
+        /// <param name="cue"></param>
+        public void playRepeat(String cue)
+        {
+            cache(cue);
+            repeat[cue] = true;
+            if (cues[cue].IsPrepared)
+            {
+                cues[cue].Play();
+            }
+            else if (cues[cue].IsPaused)
+            {
+                cues[cue].Stop(AudioStopOptions.Immediate);
+                recache(cue);
+                cues[cue].Play();
+            }
+        }
+
+		/// <summary>
+		/// Pause a playing sound.
+		/// </summary>
+		/// <param name="cue"></param>
+        public void pause(String cue)
+        {
+            cache(cue);
+            if (cues[cue].IsPlaying)
+            {
+                cues[cue].Pause();
+            }
+        }
+
+        /// <summary>
+		/// Stop a sound.
+        /// </summary>
+        /// <param name="cue"></param>
+        public void stop(String cue)
+        {
+            cache(cue);
+            if (cues[cue].IsPlaying || cues[cue].IsPaused)
+            {
+                cues[cue].Stop(AudioStopOptions.AsAuthored);
+            }
+            repeat[cue] = false;
+        }
+
+        /// <summary>
+		/// Stop all sounds.
+        /// </summary>
+        public void stopAll()
+        {
+            foreach(Cue cue in cues.Values)
+            {
+                cue.Stop(AudioStopOptions.Immediate);
+            }
+        }
+
+        /// <summary>
+		/// Cache the Cue object and whether it should be repeating.
+        /// </summary>
+        /// <param name="cue"></param>
+        private void cache(String cue)
+        {
+            if (!cues.ContainsKey(cue))
+            {
+                cues.Add(cue, soundBank.GetCue(cue));
+                repeat.Add(cue, false);
+            }
+        }
+
+        /// <summary>
+		/// Recache a new version of a cue.
+        /// </summary>
+        /// <param name="cue"></param>
+        private void recache(String cue)
+        {
+            cues[cue] = soundBank.GetCue(cue);
+        }
+
+        /// <summary>
+		/// Set a global variable in the AudioEngine.
+        /// </summary>
+        /// <param name="name">The name of the variable.</param>
+        /// <param name="value">The value of the variable.</param>
+        public void set(String name, float value)
+        {
+            audioEngine.SetGlobalVariable(name, value);
+        }
+
+        /// <summary>
+		/// Get a global variable from the AudioEngine.
+        /// </summary>
+        /// <param name="name">The name of the variable.</param>
+        /// <returns>The value of the variable.</returns>
+        public float get(String name)
+        {
+            return audioEngine.GetGlobalVariable(name);
+        }
+
+        /// <summary>
+		/// Pass the update call to the AudioEngine.
+        /// </summary>
+        public void update()
+        {
+            foreach(String cue in cues.Keys)
+            {
+                bool repeatable = repeat[cue];
+
+                if (cues[cue].IsStopped)
+                {
+                    recache(cue);
+
+                    if (repeatable)
+                    {
+                        cues[cue].Play();
+                    }
+                }
+            }
+            
+            audioEngine.Update();
+        }
     }
 }
--- GammaDraconis/GammaDraconis/Core/Input.cs
+++ GammaDraconis/GammaDraconis/Core/Input.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace GammaDraconis.Core
 {
@@ -11,9 +12,200 @@ namespace GammaDraconis.Core
     /// </summary>
     class Input
     {
+        private Dictionary<String, String> inputKeys;
+        private Dictionary<String, KeyState> keyStates;
+        private Dictionary<String, KeyState> keyStatesOld;
+        private Dictionary<String, Boolean> keyPresses;
+
+        public Vector2 mousePosition;
+
         /// <summary>
-        /// Update the manager's current state.
+        /// Initialize the Input manager's state and preferences.
         /// </summary>
-        public void update() { }
+        public Input()
+        {
+            inputKeys = new Dictionary<String, String>();
+            keyStates = new Dictionary<String, KeyState>();
+            keyPresses = new Dictionary<String, Boolean>();
+
+            // Default action assignments
+            inputKeys.Add("MenuUp", "up");
+            inputKeys.Add("MenuDown", "down");
+            inputKeys.Add("MenuLeft", "left");
+            inputKeys.Add("MenuRight", "right");
+            inputKeys.Add("MenuSelect", "enter");
+            inputKeys.Add("MenuToggle", "escape");
+
+            inputKeys.Add("Up", "down");
+            inputKeys.Add("Down", "up");
+            inputKeys.Add("Left", "left");
+            inputKeys.Add("Right", "right");
+            inputKeys.Add("Fire1", "space");
+            inputKeys.Add("Fire2", "enter");
+            inputKeys.Add("Pause", "p");
+        }
+
+        /// <summary>
+        /// Check the state of an input action.
+        /// </summary>
+        /// <param name="action">The input action.</param>
+        /// <returns>Whether or not the key that performs this action is down.</returns>
+        public Boolean inputDown(String action)
+        {
+            if (inputKeys.ContainsKey(action))
+            {
+                return keyStates[inputKeys[action]] == KeyState.Down;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Check if the action's input has been pressed.  
+        /// Distinct from inputState() in that it only returns True once for each time the key goes down.
+        /// </summary>
+        /// <param name="action">The input action.</param>
+        /// <returns>Whether or not the key that performs this action is pressed.</returns>
+        public Boolean inputPressed(String action)
+        {
+            if (inputKeys.ContainsKey(action))
+            {
+                Boolean pressed = keyPresses[inputKeys[action]];
+                keyPresses[inputKeys[action]] = false;
+                return pressed;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Update the input states.
+        /// </summary>
+        public void update()
+        {
+            // Shift keyStates to keyStatesOld
+            foreach(String key in inputKeys.Values)
+            {
+                keyStatesOld[key] = keyStates[key];
+                keyStates[key] = KeyState.Up;
+            }
+            
+            // Check keyboard inputs
+            KeyboardState keyboardState = Keyboard.GetState();
+            Keys[] pressedKeys = keyboardState.GetPressedKeys();
+
+            bool control = false;
+            bool shift = false;
+            bool alt = false;
+
+            // Handle modifier keys
+            for (int pressedKeyIndex = 0; pressedKeyIndex < pressedKeys.Length; pressedKeyIndex++)
+            {
+                String keyName = pressedKeys[pressedKeyIndex].ToString();
+
+                if (keyName == "LeftAlt" || keyName == "RightAlt")
+                {
+                    alt = true;
+                }
+
+                if (keyName == "LeftShift" || keyName == "RightShift")
+                {
+                    shift = true;
+                }
+
+                if (keyName == "LeftControl" || keyName == "RightControl")
+                {
+                    control = true;
+                }
+            }
+
+            // Handle stupid MS key names
+            for (int pressedKeyIndex = 0; pressedKeyIndex < pressedKeys.Length; pressedKeyIndex++)
+            {
+                String keyName = pressedKeys[pressedKeyIndex].ToString().ToLower();
+                if (keyName == "back")
+                {
+                    keyName = "backspace";
+                }
+                if (keyName == "oemcomma")
+                {
+                    keyName = "comma";
+                }
+
+                String modName = (control ? "control+" : "") + (alt ? "alt+" : "") + (shift ? "shift+" : "") + keyName;
+                if (keyStates.ContainsKey(keyName))
+                {
+                    keyStates[keyName] = KeyState.Down;
+                }
+                if (keyStates.ContainsKey(modName))
+                {
+                    keyStates[modName] = KeyState.Down;
+                }
+            }
+
+            // Check mouse inputs
+            MouseState mouseState = Mouse.GetState();
+
+            mousePosition = new Vector2(mouseState.X, mouseState.Y);
+
+            if (keyStates.ContainsKey("Mouse1"))
+            {
+                keyStates["Mouse1"] = (mouseState.LeftButton == ButtonState.Pressed ? KeyState.Down : KeyState.Up);
+            }
+
+            if (keyStates.ContainsKey("Mouse2"))
+            {
+                keyStates["Mouse2"] = (mouseState.RightButton == ButtonState.Pressed ? KeyState.Down : KeyState.Up);
+            }
+
+            if (keyStates.ContainsKey("Mouse3"))
+            {
+                keyStates["Mouse3"] = (mouseState.MiddleButton == ButtonState.Pressed ? KeyState.Down : KeyState.Up);
+            }
+
+            // Find new key presses
+            foreach (String key in inputKeys.Values)
+            {
+                keyPresses[key] =
+                    (keyPresses[key] || (keyStates[key] == KeyState.Down) && (keyStatesOld[key] == KeyState.Up))
+                    && (keyStates[key] == KeyState.Down);
+            }
+        }
+
+        /// <summary>
+        /// Reset keyStates and keyPresses.
+        /// </summary>
+        public void reset()
+        {
+            // Reset input dictionaries
+            keyStates = new Dictionary<String, KeyState>();
+            keyStatesOld = new Dictionary<String, KeyState>();
+            keyPresses = new Dictionary<String, Boolean>();
+
+            // Loop through all defined input actions
+            foreach(String key in inputKeys.Values)
+            {
+                // Set all inputs to up
+                keyStates.Add(key, KeyState.Up);
+                keyStatesOld.Add(key, KeyState.Up);
+
+                // Set all inputs as not pressed
+                keyPresses.Add(key, false);
+            }
+        }
+
+        /// <summary>
+        /// Get the key binding that corresponds to the given action.
+        /// </summary>
+        /// <param name="action">An action.</param>
+        /// <returns>The key binding.</returns>
+        public String getKeyBinding(String action)
+        {
+            return inputKeys[action];
+        }
     }
 }

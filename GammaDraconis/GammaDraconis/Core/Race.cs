3af119738291f53e6102fee0239366ba525aba47
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-03-29 21:15:05 +0000

    Added race manager object.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1095 ad4848e8-a12d-0410-9a8f-f544a64f585b

---
+++ GammaDraconis/GammaDraconis/Core/Race.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using GammaDraconis.Types;
+
+namespace GammaDraconis.Core
+{
+    /// <summary>
+    /// Manages a race, given a set of Racers and a Course.
+    /// </summary>
+    class Race
+    {
+        // The course for the race
+        private Course course;
+        private int length;
+        private bool loop;
+
+        // The racers and their statuses
+        private Dictionary<Racer, int> state;
+
+        /// <summary>
+        /// Create a race manager.
+        /// </summary>
+        /// <param name="course">The race course</param>
+        /// <param name="racers">The racers</param>
+        public Race(Course course, List<Racer> racers)
+        {
+            this.course = course;
+            length = course.path.Count;
+            loop = course.loop;
+
+            foreach (Racer racer in racers)
+            {
+                state.Add(racer, 0);
+            }
+        }
+
+        /// <summary>
+        /// Get an upcoming coordinate for the given racer.
+        /// </summary>
+        /// <param name="racer">The racer object</param>
+        /// <param name="offset">How many coordinates forward in the race</param>
+        /// <returns></returns>
+        public Coords coord(Racer racer, int offset)
+        {
+            int[] info = status(racer);
+        }
+        public Coords nextCoord(Racer racer) { coord(racer, 1); }
+
+        /// <summary>
+        /// Update the current status of the Race manager.
+        /// </summary>
+        public void update()
+        {
+        }
+
+        /// <summary>
+        /// Get a the lap and checkpoint 
+        /// </summary>
+        /// <param name="racer"></param>
+        /// <returns></returns>
+        private int[] status(Racer racer)
+        {
+            int status = state[racer];
+            int[] info = new int[2];
+            info[0] = status / length;
+            info[1] = status % length;
+        }
+    }
+}

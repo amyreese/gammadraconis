d41f20f6f8a028f37be415d2f17ba8b2ee6804a9
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-03-29 16:28:41 +0000

    Added function for a combined roll/yaw rotation.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1087 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- GammaDraconis/GammaDraconis/Types/GameObject.cs
+++ GammaDraconis/GammaDraconis/Types/GameObject.cs
@@ -89,5 +89,17 @@ namespace GammaDraconis.Types
             amount = -MathHelper.Clamp(amount, -1f, 1f) * 0.1f;
             acceleration.R *= Quaternion.CreateFromAxisAngle(Vector3.Up, amount);
         }
+
+        /// <summary>
+        /// Rotationally accelerate in a combined roll/yaw movement.
+        /// </summary>
+        /// <param name="amount">Stick amount clamped between -1f and 1f</param>
+        public void turn(float amount)
+        {
+            amount = -MathHelper.Clamp(amount, -1f, 1f) * 0.1f;
+            acceleration.R *= Quaternion.CreateFromAxisAngle(Vector3.Up, 0.5f * amount); // yaw
+            acceleration.R *= Quaternion.CreateFromAxisAngle(Vector3.Backward, amount);  // roll
+        }
+
     }
 }

533fcae4bb68712e5960d2dc16bd669c0954a2ec
msokol <msokol@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-04-06 14:48:02 +0000

    Added OctreeLeaf.cs to the system, but don't make use of it yet. working on fully figuring out how to do that
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1156 ad4848e8-a12d-0410-9a8f-f544a64f585b

---
+++ GammaDraconis/GammaDraconis/Video/OctreeLeaf.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GammaDraconis.Types;
+
+public class OctreeLeaf
+{
+    private const int maxobj = 8;
+    private List<GameObject> containedObjects;
+    private List<OctreeLeaf> childLeaves;
+    private BoundingBox containerBox;
+
+    public OctreeLeaf(BoundingBox bound)
+    {
+        containedObjects = new List<Geometry>();
+        childLeaves = null;
+        containerBox = bound;
+    }
+
+    public List<GameObject> ContainedObjects
+    {
+        get { return containedObjects; }
+        set { containedObjects = value; }
+    }
+
+    public List<OctreeLeaf> ChildLeaves
+    {
+        get { return childLeaves; }
+    }
+
+    public BoundingBox ContainerBox
+    {
+        get { return containerBox; }
+        set { containerBox = value; }
+    }
+
+    protected void Split()
+    {
+        Vector3 half = ContainerBox.Max - ContainerBox.Min;
+        Vector3 halfx = Vector3.UnitX * half;
+        Vector3 halfy = Vector3.UnitY * half;
+        Vector3 halfz = Vector3.UnitZ * half;
+
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min, ContainerBox.Min + half)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + halfx, ContainerBox.Max - half + halfx)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + halfz, ContainerBox.Min + half + halfz)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + halfx + halfz, ContainerBox.Max - halfy)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + halfy, ContainerBox.Max - halfx - halfz)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + halfy + halfx, ContainerBox.Max - halfz)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + halfy + halfz, ContainerBox.Max - halfx)));
+        ChildLeaves.Add(new OctreeLeaf(new BoundingBox(ContainerBox.Min + half, ContainerBox.Max)));
+    }
+   
+}

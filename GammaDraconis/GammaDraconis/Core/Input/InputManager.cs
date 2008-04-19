886e6c5bb3fe696a4ef9b9ebe10dc656e6d7cd7c
jgruenberg <jgruenberg@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-04-19 18:50:50 +0000

    Added InputManager to manage what player uses what control scheme.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1255 ad4848e8-a12d-0410-9a8f-f544a64f585b

---
+++ GammaDraconis/GammaDraconis/Core/Input/InputManager.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace GammaDraconis.Core.Input
+{
+    class InputManager
+    {
+        public enum ControlScheme
+        {
+            None, GamePad, KeyboardWASD, KeyboardNumPad
+        }
+
+        private Dictionary<PlayerIndex, ControlScheme> controlMappings;
+        private PlayerIndex mouseUser;
+
+        public InputManager()
+        {
+            controlMappings = new Dictionary<PlayerIndex, ControlScheme>();
+            AutoRegisterControlSchemes();
+        }
+
+        public void AutoRegisterControlSchemes()
+        {
+            if (GamePad.GetCapabilities(PlayerIndex.One).IsConnected)
+            {
+                controlMappings.Add(PlayerIndex.One, ControlScheme.GamePad);
+            }
+            else
+            {
+                controlMappings.Add(PlayerIndex.One, ControlScheme.KeyboardWASD);
+            }
+
+            if (GamePad.GetCapabilities(PlayerIndex.Two).IsConnected)
+            {
+                controlMappings.Add(PlayerIndex.Two, ControlScheme.GamePad);
+            }
+            else
+            {
+                if (IsSchemeAvailable(ControlScheme.KeyboardWASD))
+                {
+                    controlMappings.Add(PlayerIndex.Two, ControlScheme.KeyboardWASD);
+                }
+                else
+                {
+                    controlMappings.Add(PlayerIndex.Two, ControlScheme.KeyboardNumPad);
+                }
+            }
+
+            if (GamePad.GetCapabilities(PlayerIndex.Three).IsConnected)
+            {
+                controlMappings.Add(PlayerIndex.Three, ControlScheme.GamePad);
+            }
+            else
+            {
+                if (IsSchemeAvailable(ControlScheme.KeyboardWASD))
+                {
+                    controlMappings.Add(PlayerIndex.Three, ControlScheme.KeyboardWASD);
+                }
+                else
+                {
+                    controlMappings.Add(PlayerIndex.Three, ControlScheme.KeyboardNumPad);
+                }
+            }
+
+            if (GamePad.GetCapabilities(PlayerIndex.Four).IsConnected)
+            {
+                controlMappings.Add(PlayerIndex.Four, ControlScheme.GamePad);
+            }
+            else
+            {
+                if (IsSchemeAvailable(ControlScheme.KeyboardWASD))
+                {
+                    controlMappings.Add(PlayerIndex.Four, ControlScheme.KeyboardWASD);
+                }
+                else
+                {
+                    controlMappings.Add(PlayerIndex.Four, ControlScheme.KeyboardNumPad);
+                }
+            }
+        }
+
+        public bool IsSchemeAvailable(ControlScheme c)
+        {
+            foreach (KeyValuePair<PlayerIndex, ControlScheme> kvp in controlMappings)
+                if (kvp.Value == c)
+                    return false;
+
+            return true;
+        }
+
+        public PlayerInput GetPlayerInput(PlayerIndex p)
+        {
+            return new PlayerInput(p, controlMappings[p]);
+        }
+    }
+}
--- GammaDraconis/GammaDraconis/Core/Input/PlayerInput.cs
+++ GammaDraconis/GammaDraconis/Core/Input/PlayerInput.cs
@@ -144,5 +144,85 @@ namespace GammaDraconis.Core.Input
                 inputKeys.Add(Commands.Menu, "escape");
             }
         }
+
+        public PlayerInput(PlayerIndex playerIndex, InputManager.ControlScheme controlScheme)
+            : base(playerIndex)
+        {
+            if (controlScheme == InputManager.ControlScheme.GamePad)
+            {
+                inputAxis.Add(Commands.Yaw, "LeftX");
+                inputAxis.Add(Commands.Pitch, "LeftY");
+                inputAxis.Add(Commands.Throttle, "Triggers");
+                inputAxis.Add(Commands.CameraX, "RightX");
+                inputAxis.Add(Commands.CameraY, "RightY");
+
+                inputKeys.Add(Commands.RollLeft, "PadLB");
+                inputKeys.Add(Commands.RollRight, "PadRB");
+
+                inputKeys.Add(Commands.Fire1, "PadA");
+                inputKeys.Add(Commands.Fire2, "PadB");
+                inputKeys.Add(Commands.Pause, "PadStart");
+                inputKeys.Add(Commands.Menu, "PadBack");
+
+                inputKeys.Add(Commands.MenuUp, "PadUp");
+                inputKeys.Add(Commands.MenuDown, "PadDown");
+                inputKeys.Add(Commands.MenuLeft, "PadLeft");
+                inputKeys.Add(Commands.MenuRight, "PadRight");
+                inputKeys.Add(Commands.MenuSelect, "PadA");
+
+                inputKeys.Add(Commands.Join, "PadA");
+                inputKeys.Add(Commands.Leave, "PadBack");
+            }
+            else if (controlScheme == InputManager.ControlScheme.KeyboardWASD)
+            {
+                inputKeys.Add(Commands.Up, "up");
+                inputKeys.Add(Commands.Down, "down");
+                inputKeys.Add(Commands.Left, "left");
+                inputKeys.Add(Commands.Right, "right");
+                inputKeys.Add(Commands.RollLeft, "a");
+                inputKeys.Add(Commands.RollRight, "d");
+                inputKeys.Add(Commands.ThrottleUp, "w");
+                inputKeys.Add(Commands.ThrottleDown, "s");
+                inputKeys.Add(Commands.Fire1, "space");
+                inputKeys.Add(Commands.Fire2, "enter");
+
+                inputKeys.Add(Commands.MenuUp, "up");
+                inputKeys.Add(Commands.MenuDown, "down");
+                inputKeys.Add(Commands.MenuLeft, "left");
+                inputKeys.Add(Commands.MenuRight, "right");
+                inputKeys.Add(Commands.MenuSelect, "enter");
+
+                inputKeys.Add(Commands.Join, "w");
+                inputKeys.Add(Commands.Leave, "s");
+
+                inputKeys.Add(Commands.Pause, "p");
+                inputKeys.Add(Commands.Menu, "escape");
+            }
+            else if (controlScheme == InputManager.ControlScheme.KeyboardNumPad)
+            {
+                inputKeys.Add(Commands.Up, "numpad8");
+                inputKeys.Add(Commands.Down, "numpad2");
+                inputKeys.Add(Commands.Left, "numpad4");
+                inputKeys.Add(Commands.Right, "numpad6");
+                inputKeys.Add(Commands.RollLeft, "numpad7");
+                inputKeys.Add(Commands.RollRight, "numpad9");
+                inputKeys.Add(Commands.ThrottleUp, "numpad5");
+                inputKeys.Add(Commands.ThrottleDown, "numpad0");
+                inputKeys.Add(Commands.Fire1, "numpad1");
+                inputKeys.Add(Commands.Fire2, "numpad3");
+
+                inputKeys.Add(Commands.MenuUp, "numpad8");
+                inputKeys.Add(Commands.MenuDown, "numpad2");
+                inputKeys.Add(Commands.MenuLeft, "numpad4");
+                inputKeys.Add(Commands.MenuRight, "numpad6");
+                inputKeys.Add(Commands.MenuSelect, "numpad0");
+
+                inputKeys.Add(Commands.Join, "numpad5");
+                inputKeys.Add(Commands.Leave, "numpad0");
+
+                inputKeys.Add(Commands.Pause, "p");
+                inputKeys.Add(Commands.Menu, "escape");
+            }
+        }
     }
 }

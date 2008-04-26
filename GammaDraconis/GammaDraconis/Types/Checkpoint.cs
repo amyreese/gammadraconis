bd972b75c9e18225ceeb58627b2b4edf572dc8b6
bschroth <bschroth@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-04-26 19:15:38 +0000

    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1302 ad4848e8-a12d-0410-9a8f-f544a64f585b

---
+++ GammaDraconis/GammaDraconis/Types/Checkpoint.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GammaDraconis.Types
+{
+    class Checkpoint : GameObject
+    {
+        public Checkpoint() : base()
+        {
+            size = 50.0f;
+        }
+    }
+}

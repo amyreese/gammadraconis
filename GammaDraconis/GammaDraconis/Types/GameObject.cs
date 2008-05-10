da0506a43d426ac0a52bec3ab960570d380ef3b1
jandrusyszyn <jandrusyszyn@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-05-10 22:10:53 +0000

    Issue 40 - Mines collide
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1407 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- GammaDraconis/GammaDraconis/Types/GameObject.cs
+++ GammaDraconis/GammaDraconis/Types/GameObject.cs
@@ -46,7 +46,7 @@ namespace GammaDraconis.Types
         public int mass = 1000;
         public float dragL = 1f;
         public float dragR = 3f;
-        public float size = 1f;
+        public float size = 2f;
 
         // Camera settings
         public Coords camera;

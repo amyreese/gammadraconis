4294fee8372707bfe23cff6623e95e6dada1e7d3
pdobson <pdobson@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-05-14 19:21:22 +0000

    Asteroid Field in circle track is made.  Commented out right now because of slow down, will optimize by adjusting size of field.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/3dgames/trunk@1440 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- GammaDraconis/GammaDraconis/Lua/MapBuilders/AsteroidFields.lua
+++ GammaDraconis/GammaDraconis/Lua/MapBuilders/AsteroidFields.lua
@@ -1,8 +1,30 @@
-roid = Proto.getThing("Asteroid800A", Coords(-1500,6500,6500), Coords(0.1,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
 
-roid = Proto.getThing("Asteroid800A", Coords(-500,6500,6500), Coords(0.1,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
+function makeRoidRow(xoffset, yoffset, zoffset, gameScene)
+	roid = Proto.getThing("Asteroid800A", Coords(xoffset-2500,yoffset,zoffset), Coords(0.3 * math.random(),-0.2 * math.random(),0,0.004,0.001,0.0004))
+	gameScene:track(roid, GO_TYPE.DEBRIS)
 
-roid = Proto.getThing("Asteroid800A", Coords(0,6500,6500), Coords(0.1,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
+	roid = Proto.getThing("Asteroid800A", Coords(xoffset-1500,yoffset,zoffset), Coords(0.6 * math.random(),-0.4 * math.random(),0,0.004,0.001,0.0004))
+	gameScene:track(roid, GO_TYPE.DEBRIS)
+
+	roid = Proto.getThing("Asteroid800A", Coords(xoffset-500,yoffset,zoffset), Coords(0.01 * math.random(),-0.1 * math.random(),0,0.004,0.001,0.0004))
+	gameScene:track(roid, GO_TYPE.DEBRIS)
+
+	roid = Proto.getThing("Asteroid800A", Coords(xoffset+500,yoffset,zoffset), Coords(-0.6 * math.random(),0.4 * math.random(),0,0.004,0.001,0.0004))
+	gameScene:track(roid, GO_TYPE.DEBRIS)
+
+	roid = Proto.getThing("Asteroid800A", Coords(xoffset+1500,yoffset,zoffset), Coords(-0.3 * math.random(),0.2 * math.random(),0,0.004,0.001,0.0004))
+	gameScene:track(roid, GO_TYPE.DEBRIS)
+end
+
+
+function makeRoidPlane(xoffset, yoffset, zoffset, gameScene, columns)
+	for x=0,columns do
+		makeRoidRow(xoffset, yoffset, zoffset + (500 * x), gameScene)
+	end
+end
+
+function makeRoidCube(xoffset, yoffset, zoffset, gameScene, planes)
+	for x=0, planes do
+		makeRoidPlane(xoffset, yoffset + (500 * x), zoffset, gameScene, planes)
+	end
+end
--- GammaDraconis/GammaDraconis/Maps/CircleTrack/CircleTrack.lua
+++ GammaDraconis/GammaDraconis/Maps/CircleTrack/CircleTrack.lua
@@ -1,5 +1,5 @@
 library( "MapBuilders/Turns" )
---library( "MapBuilders/AsteroidFields" )
+library( "MapBuilders/AsteroidFields" )
 
 
 
@@ -23,50 +23,9 @@ gameScene:track(skybox, GO_TYPE.SKYBOX)
 Skybox.lights[0] = Light(Vector3(-0.05,  0.1, -1), Vector3(0.9, 0.7, 0.7), Vector3(1,1,1))
 Skybox.lights[1] = Light(Vector3( 0.95, -0.9,  1), Vector3(0.4, 0.4, 0.4), Vector3(0.5,0.5,0.5))
 
-
-roid = Proto.getThing("Asteroid800A", Coords(-1500,8500,6500), Coords(0.0,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-roid = Proto.getThing("Asteroid800A", Coords(-2500,8500,6500), Coords(0.0,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-roid = Proto.getThing("Asteroid800A", Coords(-500,8500,6500), Coords(0.0,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-roid = Proto.getThing("Asteroid800A", Coords(0,8500,6500), Coords(-0.0,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-roid = Proto.getThing("Asteroid800A", Coords(500,8500,6500), Coords(-0.0,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
---[[
-roid = Proto.getThing("Asteroid800A", Coords(3000,-5,-500), Coords(-10.5,0,0,0.003,0.004,0.0002))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-
-roid = Proto.getThing("Asteroid800A", Coords(-500,5,2600), Coords(10.5,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-
-roid = Proto.getThing("Asteroid800A", Coords(500,-5,2600), Coords(-10.5,0,0,0.003,0.004,0.0002))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-
-roid = Proto.getThing("Asteroid800A", Coords(-2000,5,500), Coords(1.5,-0,0,0.004,0.001,0.0004))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-
-roid = Proto.getThing("Asteroid800A", Coords(-3000,-5,500), Coords(-1.5,0,0,0.003,0.004,0.0002))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-
-roid = Proto.getThing("Asteroid800B", Coords(-2800,5,500), Coords(0,0,1.5, 0.001, 0.003, 0.001))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-
-
-roid = Proto.getThing("Asteroid800B", Coords(-2000,5,0), Coords(0,0,-1.5, 0.002, 0.001, 0.003))
-gameScene:track(roid, GO_TYPE.DEBRIS)
-]]--
+--makeRoidRow( 0, 8500, 6500, gameScene )
+--makeRoidPlane( -1000, 8500, 6500, gameScene, 8 )
+--makeRoidCube( -1000, 7000, 6500, gameScene, 4 )
 
 course = Course()
 Engine.GetInstance().course = course
@@ -79,10 +38,10 @@ zoffset = 500
 local trackAttributes = {radius=2500, checkpoints = 16, degreesBetweenCheckpoints = 360 / 16}
 
 --All Zero Checkpoint for proper axis relation
-table.insert( path, {x=0, y=0, z=0, pitch=0, yaw=0, roll=0} )
+--table.insert( path, {x=0, y=0, z=0, pitch=0, yaw=0, roll=0} )
 
-path = PosYToPosZ( path, xoffset, yoffset, zoffset, trackAttributes )
-path = PosZToPosY(path, xoffset, 5000, 500, trackAttributes)
+--path = PosYToPosZ( path, xoffset, yoffset, zoffset, trackAttributes )
+--path = PosZToPosY(path, xoffset, 5000, 500, trackAttributes)
 path = PosYToPosZ( path, xoffset, 5500, 5500, trackAttributes )
 --path = NegYtoPosZ( path, xoffset, yoffset, 500, trackAttributes )
 --path = NegYToNegZ(path, xoffset, -500, 2500, trackAttributes)
